27bba084a3c486bcf035b944891f19680c846acf
MuradBakhshiyeff7 <muradsb-ab109@code.edu.az>
2025-03-11 11:56:34 +0400

    commit added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("ededi daxil edin: "); task-1
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int sum = 1;
+
+            //if (number < 0)
+            //    number *= -1;
+
+            //while (number > 0)
+            //{
+            //    sum *= number % 10;
+            //    number /= 10;
+            //}
+
+            //Console.WriteLine("reqemlerin cemi budur: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+            //Console.Write("Ededi daxil edin: "); task-2
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number < 2)
+            //{
+            //    Console.WriteLine("Murekkeb eded deyil");
+            //    return;
+            //}
+
+            //bool isPrime = true;
+
+            //for (int i = 2; i * i <= number; i++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+            //    }
+            //}
+
+            //if (isPrime)
+            //    Console.Write('0');
+            //    Console.WriteLine("Sade ededdir");
+            //else
+            //    Console.WriteLine("Murekkeb ededdir");
+
+
+            //string[] words = { "alma", "armud", "banan", "qarpız", "şaftalı" }; task-3
+
+            //ReverseArray(words);
+
+            //foreach (var word in words)
+            //{
+            //    Console.Write(word + " ");
+            //}
+           
+            
+            //static void ReverseArray(string[] arr)
+            //{
+            //    int n = arr.Length;
+
+            //    for (int i = 0; i < n / 2; i++)
+            //    {
+
+            //        string temp = arr[i];
+            //        arr[i] = arr[n - i - 1];
+            //        arr[n - i - 1] = temp;
+            //    }
+            //}
+
+            //int[] arr = { 3, 5, 2, 8, 3, 9, 2 };
+
+            //FindDuplicates(arr);
+
+
+            //static void FindDuplicates(int[] arr)
+            //{
+            //    int n = arr.Length;
+
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        bool isDuplicate = false;
+
+            //        for (int j = 0; j < i; j++)
+            //        {
+            //            if (arr[i] == arr[j]) 
+            //            {
+            //                isDuplicate = true;
+            //                Console.WriteLine(arr[i]);
+            //                break;
+            //            }
+            //        }
+
+                    //if (!isDuplicate) 
+                    //{
+                    //    for (int j = i + 1; j < n; j++)
+                    //    {
+                    //        if (arr[i] == arr[j])
+                    //        {
+                    //            Console.WriteLine(arr[i]); 
+                    //            break;
+                    //        }
+                    //    }
+                    //}
+                }
+            }
+        }
+    }
+}
